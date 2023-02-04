0a2962f1df5944719b0e81277367875d8222c0d2
Rohit Bisht <rbtunes0@gmail.com>
2023-02-04 09:30:02 +0000

    2RI3P

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Keep Smiling and spread happiness. Best of luck......
-        </p>
+        <p>RIP......</p>
         <a
           className="App-link"
           href="https://reactjs.org"
